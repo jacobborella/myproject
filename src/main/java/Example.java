cdc3838439b9da2e02df59d1436bb13a6fe66090
Jacob Borella <jacobborella@hotmail.com>
2017-09-13 14:08:01 +0200

    first commit

---
+++ src/main/java/Example.java
@@ -0,0 +1,22 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+	@Value("${GREETING}")
+	private String greeting;
+	
+    @RequestMapping("/")
+    String home() {
+        return greeting;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
