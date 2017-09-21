ece1302524f68ef0d86638288a20a0403ff03a2c
Jacob Borella <jacobborella@hotmail.com>
2017-09-22 00:11:11 +0200

    first version for presentation

--- ansible/library/oc_template.py
+++ ansible/library/oc_template.py
@@ -27,13 +27,18 @@ def main():
     #calc md5 for new file https://gist.github.com/aseigneurin/4902819f17218340d11f
     #label template with md5
     #compare md5 by getting label - compare to new file
+    #light version is using md5 for tmp file if file doesn't exists file have changed (not as secure obviously)
     template_changed = True
 
     #if not and template must be there, create the template
     if not exists and 'present' == module.params['state']: 
       proc = Popen(['oc', 'create', '-f', module.params['filename'], '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
       stdout, stderr = proc.communicate()
-      changed = True
+      if proc.returncode == 0:
+        changed = True
+      else:
+        module.fail_json(msg=stdout)
+        
     if exists and template_changed:
       proc = Popen(['oc', 'replace', '-f', module.params['filename'], '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
       stdout, stderr = proc.communicate()
