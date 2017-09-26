e571530fd11de01be6ec141e41c7f3bca7644c0b
Jacob Borella <jacobborella@hotmail.com>
2017-09-27 01:07:19 +0200

    added template support

--- ansible/library/oc_template.py
+++ ansible/library/oc_template.py
@@ -4,6 +4,13 @@ from ansible.module_utils.basic import *
 from subprocess import call, Popen
 import yaml
 
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
 def main():
     fields = {
         "filename": {"required": True, "type": "str"},
@@ -26,6 +33,8 @@ def main():
 
     #if not and template must be there, create the template
     if 'present' == module.params['state']:
+      #extract new template version
+      newTemplateVersion = md5(module.params['filename'])
       if exists:
         #investigate whether the template has changed since last time if it exists
         proc = Popen(['oc', 'get', '-f', module.params['filename'], '-o', 'yaml', '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -33,27 +42,21 @@ def main():
         if proc.returncode == 0:
           #extract version of currently used template
           templateDoc = yaml.load(stdout)
-          oldTemplateVersion = templateDoc['labels']['template-version']
+          oldTemplateVersion = templateDoc['metadata']['labels']['template-version']
           response['old-template-version'] = oldTemplateVersion
-
-          newTemplateVersion = ''
-          #extract new template version
-          with open(module.params['filename'], 'r') as stream:
-            try:
-              templateDoc = yaml.load(stream)
-              newTemplateVersion = templateDoc['labels']['template-version']
-              response['new-template-version'] = newTemplateVersion
-            except yaml.YAMLError as exc:
-              module.fail_json(msg=exc)
         else:
           module.fail_json(msg=stdout)
         
         if oldTemplateVersion != newTemplateVersion:
           proc = Popen(['oc', 'apply', '-f', module.params['filename'], '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
           stdout, stderr = proc.communicate()
+          proc = Popen(['oc', 'label', '-f', module.params['filename'], '-n', module.params['namespace'], 'template-version=' + str(newTemplateVersion)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+          stdout, stderr = proc.communicate()
           changed = True
       else: 
         proc = Popen(['oc', 'create', '-f', module.params['filename'], '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = proc.communicate()
+        proc = Popen(['oc', 'label', '-f', module.params['filename'], '-n', module.params['namespace'], 'template-version=' + str(newTemplateVersion)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         stdout, stderr = proc.communicate()
         if proc.returncode == 0:
           changed = True
