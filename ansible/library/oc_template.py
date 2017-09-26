698fceb22d89fec3949c7de8f48ef93b4f785313
Jacob Borella <jacobborella@hotmail.com>
2017-09-27 00:05:30 +0200

    added template support

--- ansible/library/oc_template.py
+++ ansible/library/oc_template.py
@@ -2,6 +2,7 @@
 
 from ansible.module_utils.basic import *
 from subprocess import call, Popen
+import yaml
 
 def main():
     fields = {
@@ -23,32 +24,48 @@ def main():
     stdout, stderr = proc.communicate()
     exists = proc.returncode == 0
 
-    #investigate whether the template has changed since last time if it exists
-    #calc md5 for new file https://gist.github.com/aseigneurin/4902819f17218340d11f
-    #label template with md5
-    #compare md5 by getting label - compare to new file
-    #light version is using md5 for tmp file if file doesn't exists file have changed (not as secure obviously)
-    template_changed = True
-
     #if not and template must be there, create the template
-    if not exists and 'present' == module.params['state']: 
-      proc = Popen(['oc', 'create', '-f', module.params['filename'], '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-      stdout, stderr = proc.communicate()
-      if proc.returncode == 0:
-        changed = True
-      else:
-        module.fail_json(msg=stdout)
-        
-    if exists and template_changed:
-      proc = Popen(['oc', 'replace', '-f', module.params['filename'], '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-      stdout, stderr = proc.communicate()
-      changed = True
+    if 'present' == module.params['state']:
+      if exists:
+        #investigate whether the template has changed since last time if it exists
+        proc = Popen(['oc', 'get', '-f', module.params['filename'], '-o', 'yaml', '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = proc.communicate()
+        if proc.returncode == 0:
+          #extract version of currently used template
+          templateDoc = yaml.load(stdout)
+          oldTemplateVersion = templateDoc['labels']['template-version']
+          response['old-template-version'] = oldTemplateVersion
 
-    if 'absent' == module.params['state']:
+          newTemplateVersion = ''
+          #extract new template version
+          with open(module.params['filename'], 'r') as stream:
+            try:
+              templateDoc = yaml.load(stream)
+              newTemplateVersion = templateDoc['labels']['template-version']
+              response['new-template-version'] = newTemplateVersion
+            except yaml.YAMLError as exc:
+              module.fail_json(msg=exc)
+        else:
+          module.fail_json(msg=stdout)
+        
+        if oldTemplateVersion != newTemplateVersion:
+          proc = Popen(['oc', 'apply', '-f', module.params['filename'], '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+          stdout, stderr = proc.communicate()
+          changed = True
+      else: 
+        proc = Popen(['oc', 'create', '-f', module.params['filename'], '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = proc.communicate()
+        if proc.returncode == 0:
+          changed = True
+        else:
+          module.fail_json(msg=stdout)
+
+    if 'absent' == module.params['state'] and exists:
       proc = Popen(['oc', 'delete', '-f', module.params['filename'], '-n', module.params['namespace']], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
       stdout, stderr = proc.communicate()
       changed = True
-       
+
+      
 
     module.exit_json(changed=changed, meta=response)
 
