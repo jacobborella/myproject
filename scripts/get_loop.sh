cf7b85274f5c252bf092b84d003576fdd3d91b53
Jacob Borella <jacobborella@hotmail.com>
2017-10-18 09:17:33 +0200

    Ready for DockerCon

--- scripts/get_loop.sh
+++ scripts/get_loop.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+url=http://hello-world-demo.collectalot.org/
+#url=http://hello-world-ansibledemo.40.113.10.216.nip.io/
 for i in {1..100000}
 do
    echo -ne $i
    echo -ne ' '
-   curl http://hello-world-demo.collectalot.org/
+   curl $url
    echo ''
-   sleep 1
+   sleep 2
 done
 
